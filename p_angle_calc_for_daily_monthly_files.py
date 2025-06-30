341dd2c64e9894f014659be77b1e4cb9f8e8b034
rdiucaa <91477298+rdiucaa@users.noreply.github.com>
2025-06-30 17:09:54 +0530

    Update p_angle_calc_for_daily_monthly_files.py
    p angle calc can take input both daily and monthly files

--- p_angle_calc_for_daily_monthly_files.py
+++ p_angle_calc_for_daily_monthly_files.py
@@ -1,224 +1,144 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 19 10:15:52 2024
+Created on Mon Jun 30 14:20:30 2025
 
 @author: rushikesh
+
+# new p angle calculator script
+
+DESCRIPTION
+- Find P-angle of image based on UTC of image.
+- Inputs- * UTC time of image.
+          * Camera Kernel from specific data packet. This function can process both daily and monthly .bc files
+- Output- (float) CROTA2 in degrees. -ve value means image is rotated clockwise.
+
+If the queried time is not in the coverage the code looks for nearest coverage time and provides the p angle of that time.
+Checked correct output for monthly and daily files 
 """
 
-
-import math
-import datetime
-import spiceypy 
 import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-import matplotlib.dates as mdates
-import os
+import spiceypy
 
 
-
-# bc_file_name = '21_dec/SUT81N18P1AL10013308NNNN23355050409296_UNP_9999_999999_00_qib.bc'
-# bc_file_name = '17_may_2024/SUT81N18P1AL10022808NNNN24139075037459_T24_0728_000381_00_qib.bc'
-# bc_file_name = '27_may_2024/SUT81N18P1AL10021908NNNN24149071702658_T24_0774_000391_00_qib.bc'
-# bc_file_name = '18_june_2024_full/al1_spice_att_18june.bc'
-
-# bc_file_name = '21_june_2024/SUT81N18P1AL10005708NNNN24174072212502_T24_0876_000423_00_qib.bc'
-# bc_file_name = '20_sept_2024/SUT81N18P1AL10000108NNNN24265073807855_T24_1310_000543_00_qib.bc'
-# bc_file_name = '31_july_2024/SUT81D32P1AL10000608NNNN24214081056023_T24_1022_000490_00_qib.bc'
-# bc_file_name = '22_aug_2024/SUT81D32P1AL10003408NNNN24236081135165_C24_0329_000500_00_qib.bc'
-bc_file_name = '30_oct_2024/SUT81N18P1AL10019108NNNN24305053620769_C24_0409_000630_00_qib.bc'
-# bc_file_name = '01_oct_2024/SUT81N18P1AL10000008NNNN24276063310634_T24_1457_000577_00_qib.bc'
-# bc_file_name = '12_sept_2024/SUT81N18P1AL10003808NNNN24257072939579_C24_0338_000509_00_qib.bc'
-
-# bc_file_name = 'al1_june_full_spice_attitude.bc'
+utc_fits = '2025-05-3T00:22:16.873'
+# 
+# utc_fits = '2024-07-30T11:21:26.96925'
 
 
+# utc_fits = '2024-06-20T11:21:26.96925'
 
-# bsp_file_name = '21_dec/SUTXXN18P1AL10013308NNNN23355050409296_UNP_9999_999999_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '21_dec/SUTXXN18P1AL10013308NNNN23355050409296_UNP_9999_999999_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '17_may_2024/SUTXXN18P1AL10022808NNNN24139075037459_T24_0728_000381_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '27_may_2024/SUTXXN18P1AL10021908NNNN24149071702658_T24_0774_000391_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '18_june_2024_full/full_day_bsp/traj_master_al1.bsp'
-# bsp_file_name = '21_june_2024/SUTXXN18P1AL10005708NNNN24174072212502_T24_0876_000423_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '20_sept_2024/SUTXXN18P1AL10000108NNNN24265073807855_T24_1310_000543_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '31_july_2024/SUTXXD32P1AL10000608NNNN24214081056023_T24_1022_000490_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '22_aug_2024/SUTXXD32P1AL10003408NNNN24236081135165_C24_0329_000500_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '30_oct_2024/SUTXXN18P1AL10019108NNNN24305053620769_C24_0409_000630_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '01_oct_2024/SUTXXN18P1AL10000008NNNN24276063310634_T24_1457_000577_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '12_sept_2024/SUTXXN18P1AL10003808NNNN24257072939579_C24_0338_000509_V1_1/traj_master_al1.bsp'
-# bsp_file_name = '01/
-
-# utc_fits = '2024-05-17T22:30:00.000'
-# utc_fits = '2024-05-27T23:03:00.000'
-# utc_fits = '2023-12-20T21:30:00.000'
-# utc_fits = '2024-06-18T03:56:02.493'
-# utc_fits = '2024-06-21T11:56:02.493'
-# utc_fits = '2024-09-20T21:42:49.656'
-# utc_fits = '2024-07-31T00:02:49.656'
-# utc_fits = '2024-08-22T00:20:49.656'
-utc_fits = '2024-10-30T01:20:49.656'
-# utc_fits = '2024-10-01T06:20:49.656'
-# utc_fits = '2024-09-12T00:42:49.656'
+# File paths
+# bc_file_name = '/home/rushikesh/Downloads/SUIT_files/SPICE/p_angle_problem/aug_full_month_ck/al1_aug_full_spice_attitude.bc'
+bc_file_name = '/home/rushikesh/Downloads/SUIT_files/SPICE/kernels/ck/03_may_2025/SUT81N18P1AL10013008NNNN25124042236913_C25_0113_000910_00_qib.bc'
+# bc_file_name = '/home/rushikesh/Downloads/SUIT_files/SPICE/p_angle_problem/july_full_month_ck/al1_july_full_spice_attitude.bc'
+# bc_file_name = '/home/rushikesh/Downloads/SUIT_files/SPICE/p_angle_problem/june_full_month_ck/al1_june_full_spice_attitude.bc'
 
 
-def find_p_ang(bc,utc_fits):
+def calc_p_ang_daily_monthly(bc_file_name,utc_fits):
     
-    sc_id = -156001;    #spacecraft NAIF ID
-    sc_obj_id = -156;
-    
-    spk = '../kernels/spk/de432s.bsp'
-    pck = '../kernels/pck/pck00011_n0066.tpc'
-    # sclk = '../kernels/sclk/solo_ANC_soc-sclk_20231003_V01.tsc'
-    sclk = '../kernels/sclk/clockgen_sclk_al1.tsc'
-    bc = '../kernels/ck/'+ bc_file_name 
-    # bc = '/home/rushikesh/Downloads/SUIT_files/SPICE/p_angle_problem/nov_some_days_bc/' + bc_file_name
-    # bsp = '../kernels/spk/'+ bsp_file_name
-    lsk = '../kernels/lsk/naif0012.tls.txt'   
-    # fk = '../kernels/fk/ADITYA_frame_kernel_v03.tf' 
-    fk = '../kernels/fk/ADITYA_frame_kernel_v04.tf' 
-    
-            
-    if spk:
-        spiceypy.furnsh(spk);
-    else:
-        print('SPK not found!');
-    
-    if pck:
-        spiceypy.furnsh(pck);
-    else:
-        print('PCK not found!');    
-    if sclk:
-        spiceypy.furnsh(sclk);
-    else:
-        print('SCLK not found');
-    if bc:
-        spiceypy.furnsh(bc);
-    else:
-        print('BC not found');
-    # if bsp:
-    #     spiceypy.furnsh(bsp);
-    # else:
-    #     print('BSP not found!');
-    if lsk:
-        spiceypy.furnsh(lsk);
-    else:
-        print('LSK not found!');
-    if fk:
-        spiceypy.furnsh(fk);
-    else:
-        print('LSK not found!');    
+    try:
+        # Clear any previous SPICE kernels
+        spiceypy.kclear()
+        # Load necessary kernels
+        spiceypy.furnsh(bc_file_name)
+        spiceypy.furnsh('../kernels/spk/de432s.bsp')
+        spiceypy.furnsh('../kernels/pck/pck00011_n0066.tpc')
+        spiceypy.furnsh('../kernels/sclk/clockgen_sclk_al1.tsc')
+        spiceypy.furnsh('../kernels/lsk/naif0012.tls.txt')
+        spiceypy.furnsh('../kernels/fk/ADITYA_frame_kernel_v05.tf')
         
-        
-    if spk and pck and sclk and bc and lsk and fk :
-    # if spk and pck and sclk and bc and bsp and lsk and fk :
-        print('all kernels present')
-        
-        # string UTC to et conversion
-        et_now = spiceypy.str2et(utc_fits)
-        
-        int_et_now = int(et_now)
-        print('int_et_now = ',int_et_now)
-        
-        # C file coverage
-        coverage = spiceypy.ckcov ( bc,sc_id,False,"SEGMENT",  0.0,  "TDB")
-        # print('coverage =',coverage)
-        
-        # coverage of C kernel
-        begin_c,end_c = spiceypy.wnfetd(coverage,0)
-        # print('begin_c = ', begin_c)
-        begin_c_int = int(begin_c)
-        # print('end_c = ', end_c)
-    
-        cov_c = int(end_c-begin_c)
-        print('c = ',cov_c)
-        
-        t_vec = np.linspace(coverage[0],coverage[1],cov_c);
-        print('len_tvec = ',len(t_vec))
-        t_req = int_et_now - begin_c_int
+        # Get coverage time
+        coverage = spiceypy.ckcov(bc_file_name, -156001, False, "SEGMENT", 0.0, "TDB")
+        card_num = coverage.card
+        # print('card_num =', card_num)
 
+        def create_cov_arrays(card_num):
+            # Create the start_cov_arr with numbers from 0 to card_num - 2, with a step of 2
+            start_cov_arr = list(range(0, card_num, 2))
+            # Create the end_cov_arr with numbers from 1 to card_num - 1, with a step of 2
+            end_cov_arr = list(range(1, card_num, 2))
+            return start_cov_arr, end_cov_arr
         
-        utc_start,utc_end = spiceypy.et2utc(coverage, 'ISOC', 3);
-        print('utc_start = ', utc_start)
-        print('utc_start = ', utc_end)
+        cov_start,cov_end = create_cov_arrays(card_num) # coverage index list
+        # print('cov_start = ', cov_start)
+        # print('cov_end = ', cov_end)
+        utc_start_t_list = []
+        cov_end_t_list = []
         
-        print('t_req = ',t_req)
-        print(t_vec[t_req])
+        for i,j in zip(cov_start,cov_end): # list of et corresponding to the start and end times of a coverage window
+            utc_start_t_list.append(coverage[i])
+            cov_end_t_list.append(coverage[j])
+            
+        # print('utc_start_t_list = ', utc_start_t_list)
+        # print('cov_end_t_list = ', cov_end_t_list)
         
-        # divide the et duration into small intervals and find the state and other values:
-        t_bin = 1; #in seconds
-        t_vec = np.linspace(coverage[0],coverage[1],int((coverage[1]- coverage[0])/t_bin));
+        
+        
+        et_now = spiceypy.str2et(utc_fits) # UTC to et conversion
+        # print('et_now = ', et_now)
+        
+        def find_or_closest(x, list1, list2):
+            # First pass: check if x lies strictly between each pair
+            for a, b in zip(list1, list2):
+                if a < x < b:
+                    return x
+        
+            # Second pass: check between list2[n] and list1[n+1]
+            best = 'not_in_coverage_range'
+            best_dist = float('inf')
+            for i in range(len(list2) - 1):
+                lo = list2[i]
+                hi = list1[i + 1]
+                if lo < x < hi:
+                    # determine which bound is closest to x
+                    d_lo = x - lo
+                    d_hi = hi - x
+                    candidate = lo if d_lo <= d_hi else hi
+                    dist = d_lo if d_lo <= d_hi else d_hi
+                    if dist < best_dist:
+                        best_dist = dist
+                        best = candidate
+        
+            return best
+        
+        # find the closest time to the input utc time if the input time does 
+        # not fall within the coverage (for monthly files with more than 1 coverage windows)
+        res = find_or_closest(et_now, utc_start_t_list, cov_end_t_list)  
+        if res != 'not_in_coverage_range':
+            corrected_et = res
+        else:
+            print(res)
+            
+        
+        vec_roll_suit = np.array([0.0, 1.0, 0.0])
+        hci_sun_north = np.array([0.0, 0.0, 1.0])  # Solar north in HCI frame (Z-axis)
+        
+        
+        # utc = spiceypy.et2utc(corrected_et, 'ISOC',5)
+        # print('utc = ', utc)
+        
+        # Transform HCI to SUIT frame
+        cmat3 = spiceypy.pxform('HCI', 'ADITYA', corrected_et)
+        solar_north_in_suitframe = spiceypy.mxv(cmat3, hci_sun_north)
+        
+        # Project onto CCD plane
+        solar_north_suitccd = np.array([0, solar_north_in_suitframe[1], solar_north_in_suitframe[2]])
+        
+        # Calculate P angle
+        p_angle_radian = spiceypy.vsep(vec_roll_suit, solar_north_suitccd)
+        p_angle_deg = spiceypy.dpr() * p_angle_radian
+        if solar_north_suitccd[2] < 0: # positive and neg p angle correction as per the orbit
+            p_angle_deg = -p_angle_deg
+        
+        
+        print('p_angle_deg = ', p_angle_deg)
+        spiceypy.kclear()
+        return p_angle_deg
+    except:
+        return 'incorrect_bc_kernel_file'
     
-        
-        if  begin_c < et_now < end_c :
-            print('within ck coverage')
-            
-           
-           
-            #define suit axes in its frame:
-            vec_yaw_suit = np.array([1.0, 0.0, 0.0]);   vec_roll_suit = np.array([0.0, 1.0, 0.0]);vec_pitch_suit = np.array([0.0, 0.0, 1.0])
-            
-            # solar north in HCI frame....Z axis
-            hci_sun_north = np.array([0.0, 0.0, 1.0]);
-            # yaw2_angle_deg = np.zeros([len(t_vec),1]); roll2_angle_deg = np.zeros([len(t_vec),1]); pitch2_angle_deg = np.zeros([len(t_vec),1]);
-            
-            utc_time=spiceypy.et2utc(t_vec, 'ISOC', 3);
-            datetime_obj=[datetime.strptime(time,'%Y-%m-%dT%H:%M:%S.%f') for time in utc_time]
-            # print('datetime_obj = ',datetime_obj)
+if __name__ == "__main__":   
+    p_angle_deg = calc_p_ang_daily_monthly(bc_file_name,utc_fits)    
 
-            # start_time = min(datetime_obj)
-            # end_time = start_time + timedelta(minutes=5)
-            
     
-                
-            # cmat2 = spiceypy.pxform('ADITYA_SUIT2','HCI',t_vec[t_req]);
-           
-    
-            # vec_yaw_sun = spiceypy.mxv(cmat2,vec_yaw_suit );
-            # yaw_angle_radian = spiceypy.vsep(vec_yaw_sun, hci_sun_north);
-            # yaw2_angle_deg = spiceypy.dpr()*yaw_angle_radian
-            # print('yaw_angle =',  yaw2_angle_deg)
-            
-            # vec_roll_sun = spiceypy.mxv(cmat2,vec_roll_suit );
-            # roll_angle_radian = spiceypy.vsep(vec_roll_sun, hci_sun_north);
-            # roll2_angle_deg = spiceypy.dpr()*roll_angle_radian
-            # # print('p_angle =',  roll2_angle_deg)
-            
-            
-            # vec_pitch_sun = spiceypy.mxv(cmat2,vec_pitch_suit );
-            # pitch_angle_radian = spiceypy.vsep(vec_pitch_sun, hci_sun_north);
-            # pitch2_angle_deg = spiceypy.dpr()*pitch_angle_radian;
-            # print('pitch2_angle_deg =',  pitch2_angle_deg)
-           
-            
-           # modified method for p-angle:
-           # Calculate everything in SUIT CCD frame
-            cmat3 = spiceypy.pxform('HCI','ADITYA_SUIT2',t_vec[t_req]);
-            # cmat3 = spiceypy.pxform('HCI','ADITYA',t_vec[t_req]);
-            # get solar north in SUIT frame using the above transformation
-            solar_north_in_suitframe = spiceypy.mxv(cmat3,hci_sun_north );
-            print('solar_north_in_suitframe = ', solar_north_in_suitframe)
-            # project the solar north in suitframe onto the CCD plane :
-            # considering that the CCD plane is closely aligned with ROLL-PITCH plane and YAW is the boresight
-            solar_north_suitccd = np.array([0,solar_north_in_suitframe[1],solar_north_in_suitframe[2]]);
-            print('solar_north_suitccd = ', solar_north_suitccd)
-
-                
-            # the separation between suit roll vector(which should align with CCD columns) and solar_north_suitccd : p-angle
-            p_angle_radian = spiceypy.vsep(vec_roll_suit, solar_north_suitccd);
-            print('p_angle_radian = ', p_angle_radian)
-            p_angle_deg = spiceypy.dpr()*p_angle_radian
-            # print('p_angle =',  p_angle_deg)
-            print(solar_north_suitccd[2])
-            if float(solar_north_suitccd[2]) < 0:
-                p_angle_deg = -1 * p_angle_deg
-            print('p_angle =',  p_angle_deg)
-            
-            spiceypy.kclear()
-            return(p_angle_deg)    
-         
-# p_ang =  find_p_ang(bc_file_name,bsp_file_name,utc_fits)       
-p_ang =  find_p_ang(bc_file_name,utc_fits)          
-            
-            
